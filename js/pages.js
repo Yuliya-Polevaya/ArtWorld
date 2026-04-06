e6b2d498c52589a72e004b100455c986a1c04801
Yuliya-Polevaya <yulya.polevaya07.polevaya@mail.ru>
2026-04-07 01:42:13 +0300

    изменения

--- js/pages.js
+++ js/pages.js
@@ -350,10 +350,10 @@ document.getElementById("subscribeForm").addEventListener("submit", async e => {
         return;
     }
 
-    if (!/^\+7 \d{3}-\d{3}-\d{2}-\d{2}$/.test(telephone)) {
-        alert("Неправильный номер телефона!");
-        return;
-    }
+    // if (!/^\+7 \d{3}\d{3}\d{2}-\d{2}$/.test(telephone)) {
+    //     alert("Неправильный номер телефона!");
+    //     return;
+    // }
 
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
         alert("Неправильный email!");
