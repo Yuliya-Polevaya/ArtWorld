e7f38e29d4b251d21f1fc1ff86c77ed040a05b37
Yuliya-Polevaya <yulya.polevaya07.polevaya@mail.ru>
2026-04-08 11:32:29 +0300

    все изменения

--- js/pages.js
+++ js/pages.js
@@ -1,20 +1,24 @@
 async function fetchData() {
-    const response = await fetch("http://localhost/ArtWorld/myserver/get.php");
+    const response = await fetch("http://localhost/myserver/get.php");
     return await response.json();
 }
 
-// ------------------ Художники ------------------
+// Художники
 function loadArtists(data) {
+    // создаем контейнер для картин
     const container = document.querySelector(".artists");
-
+// Поверяем есть ли контейнер и данные в нем
     if (container && data.artists) {
+        // Отчищаем
         container.innerHTML = "";
 
         data.artists.forEach(a => {
             const card = document.createElement("div");
+            // смотрим есть ли блок
             card.className = "artists_card";
+            // сохраняем id
             card.dataset.id = a.id;
-
+            // создаем карточку
             card.innerHTML = `
                 <img src="${a.photo_artist || 'default_artist.jpg'}">
                 <div class="card_body">
@@ -22,38 +26,37 @@ function loadArtists(data) {
                     <p>${a.country || "-"}</p>
                 </div>
             `;
-
+            // открываем при нажатии страничку
             card.onclick = () => {
                 localStorage.setItem("artistId", a.id);
                 window.location.href = "artist.html";
             };
-
             container.appendChild(card);
         });
     }
-
+    // создание странички
     const artistId = localStorage.getItem("artistId");
     if (artistId && data.artists) {
         const artist = data.artists.find(a => a.id == artistId);
         if (!artist) return;
-
+        // достаем имя и вствляем как заголовок
         if (document.getElementById("art_name"))
             document.getElementById("art_name").textContent = artist.art_name;
-
+        // вставляем фото
         const img = document.querySelector(".photo_artist img");
         if (img) img.src = artist.photo_artist || 'default_artist.jpg';
-
+        // заполняем данными поля если  нет данных то -
         document.querySelectorAll(".info .art_name p").forEach(el => el.textContent = artist.art_name);
         document.querySelectorAll(".info .date_of_birth p").forEach(el => el.textContent = artist.date_of_birth || "-");
         document.querySelectorAll(".info .date_of_death p").forEach(el => el.textContent = artist.date_of_death || "-");
         document.querySelectorAll(".info .country p").forEach(el => el.textContent = artist.country || "-");
-
+        // заполняем биографию
         const bio = document.querySelector(".biography p");
         if (bio) bio.textContent = artist.biography || "-";
     }
 }
 
-// ------------------ Картины ------------------
+// Картины
 function loadPaintings(data) {
     const container = document.querySelector(".paintings");
 
@@ -109,7 +112,7 @@ function loadPaintings(data) {
     }
 }
 
-// ------------------ Стили ------------------
+//  Стили
 function loadStyles(data) {
     const container = document.querySelector(".styles");
 
@@ -141,21 +144,21 @@ function loadStyles(data) {
         const s = data.styles.find(x => x.id == id);
         if (!s) return;
 
-        if (document.getElementById("name"))
-            document.getElementById("name").textContent = s.name;
+        if (document.getElementById("name_styles"))
+            document.getElementById("name_styles").textContent = s.name;
 
         const img = document.querySelector(".photo_styles img");
         if (img) img.src = s.photo_styles || 'default_style.jpg';
 
-        document.querySelectorAll(".info .name p").forEach(el => el.textContent = s.name);
-        document.querySelectorAll(".info .period p").forEach(el => el.textContent = s.period || "-");
+        document.querySelectorAll(".info_styles .name_styles p").forEach(el => el.textContent = s.name);
+        document.querySelectorAll(".info_styles .period_styles p").forEach(el => el.textContent = s.period || "-");
 
-        const desc = document.querySelector(".description p");
+        const desc = document.querySelector(".description_styles p");
         if (desc) desc.textContent = s.description || "-";
     }
 }
 
-// ------------------ Музеи ------------------
+//  Музеи 
 function loadMuseums(data) {
     const container = document.querySelector(".museums");
 
@@ -188,22 +191,22 @@ function loadMuseums(data) {
         const m = data.museums.find(x => x.id == id);
         if (!m) return;
 
-        if (document.getElementById("name"))
-            document.getElementById("name").textContent = m.name;
+        if (document.getElementById("name_museum"))
+            document.getElementById("name_museum").textContent = m.name;
 
         const img = document.querySelector(".photo_museums img");
         if (img) img.src = m.photo_museums || 'default_museum.jpg';
 
-        document.querySelectorAll(".info .name p").forEach(el => el.textContent = m.name);
-        document.querySelectorAll(".info .city p").forEach(el => el.textContent = m.city || "-");
-        document.querySelectorAll(".info .country p").forEach(el => el.textContent = m.country || "-");
+        document.querySelectorAll(".info_museum .name_museum p").forEach(el => el.textContent = m.name);
+        document.querySelectorAll(".info_museum .city_museum p").forEach(el => el.textContent = m.city || "-");
+        document.querySelectorAll(".info_museum .country_museum p").forEach(el => el.textContent = m.country || "-");
 
-        const desc = document.querySelector(".description p");
+        const desc = document.querySelector(".description_museum p");
         if (desc) desc.textContent = m.description || "-";
     }
 }
 
-// ------------------ Жанры ------------------
+//  Жанры 
 function loadGenres(data) {
     const container = document.querySelector(".genres");
 
@@ -235,18 +238,18 @@ function loadGenres(data) {
         const g = data.genres.find(x => x.id == id);
         if (!g) return;
 
-        if (document.getElementById("name"))
-            document.getElementById("name").textContent = g.name;
+        if (document.getElementById("name_genres"))
+            document.getElementById("name_genres").textContent = g.name;
 
         const img = document.querySelector(".photo_genres img");
         if (img) img.src = g.photo_genres || 'default_genre.jpg';
 
-        const desc = document.querySelector(".description p");
+        const desc = document.querySelector(".description_genres p");
         if (desc) desc.textContent = g.description || "-";
     }
 }
 
-// ------------------ Коллекции ------------------
+//  Коллекции 
 function loadCollections(data) {
     const container = document.querySelector(".private_collection");
 
@@ -309,9 +312,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 // Сообщения
 document.getElementById("contactForm").addEventListener("submit", async e => {
     e.preventDefault();
-
+    // данные от пользователя
     const mail_client = document.getElementById("mail_client").value;
-    
+    // создаем FormData
     const formData = new FormData();
     formData.append("mail_client", document.getElementById("mail_client").value);
     formData.append("message", document.getElementById("message").value);
@@ -320,31 +323,33 @@ document.getElementById("contactForm").addEventListener("submit", async e => {
         alert("Неправильный email!");
         return;
     }
-
-    await fetch("http://localhost/ArtWorld/myserver/post_message.php", {
+    // отправка данных на сервер
+    await fetch("http://localhost/myserver/post_message.php", {
         method: "POST",
         body: formData
     });
 
     alert("Сообщение отправлено!");
+    // очистка формы
     document.getElementById("contactForm").reset();
 });
 
 // Подписка
 document.getElementById("subscribeForm").addEventListener("submit", async e => {
     e.preventDefault();
-
+    // получаем данные от пользователя
     const sub_name = document.getElementById("sub_name").value;
     const telephone = document.getElementById("sub_telephone").value;
     const email = document.getElementById("sub_email").value;
-
+    //  создаем FormData
     const formData = new FormData();
     formData.append("name", document.getElementById("sub_name").value);
     formData.append("telephone", document.getElementById("sub_telephone").value);
     formData.append("email", document.getElementById("sub_email").value);
+    // прием данных из interesting и radio
     formData.append("frequency", document.querySelector('input[name="frequency"]:checked')?.value || "");
     formData.append("interesting", document.getElementById("interesting").value);
-
+    // валедация форм
     if (!/^[А-Я][а-я]+ [А-Я][а-я]+ [А-Я][а-я]+$/.test(sub_name)) {
         alert("ФИО должно быть в формате: Иванов Иван Иванович");
         return;
@@ -359,12 +364,13 @@ document.getElementById("subscribeForm").addEventListener("submit", async e => {
         alert("Неправильный email!");
         return;
     }
-
-    await fetch("http://localhost/ArtWorld/myserver/post_mailing", {
+    // отправка данных на сервер
+    await fetch("http://localhost/myserver/post_mailing.php", {
         method: "POST",
         body: formData
     });
 
     alert("Вы подписались на рассылку!");
+    // очистка формы
     e.target.reset();
 });
